53c52acb7b93af9ca4350eb29cbcb059b87d573b
Andrew Baillie <andrewbaillie@me.com>
2016-09-24 21:45:37 +0100

    Preparing for a overlay

--- theme/js/script.js
+++ theme/js/script.js
@@ -1,11 +1,25 @@
 /*!
  * Portfolio
- * Build date: 2016-09-18 21:18:57 GMT+0100
+ * Build date: 2016-09-24 21:29:20 GMT+0100
  */
 $( document ).ready(function() {
 	
 	$('.portfolio').packery({
 		 percentPosition: true
 	});
+
+	$(document).on( 'click' , '.item' , function(e) {
+
+		e.preventDefault();
+
+		if ( $('.portfolio').hasClass('selected') ) {
+			$('.portfolio').removeClass('selected');
+			$('.item').removeClass('highlight');
+		} else {
+			$('.portfolio').addClass('selected');
+			$(this).addClass('highlight');
+		}
+
+	} );
 
 });
--- theme/js/src/script.js
+++ theme/js/src/script.js
@@ -4,4 +4,18 @@ $( document ).ready(function() {
 		 percentPosition: true
 	});
 
+	$(document).on( 'click' , '.item' , function(e) {
+
+		e.preventDefault();
+
+		if ( $('.portfolio').hasClass('selected') ) {
+			$('.portfolio').removeClass('selected');
+			$('.item').removeClass('highlight');
+		} else {
+			$('.portfolio').addClass('selected');
+			$(this).addClass('highlight');
+		}
+
+	} );
+
 });
