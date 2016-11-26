c2c28f36ea23fab39278fffc3e89be0b0f25b1a6
Andrew Baillie <andrewbaillie@me.com>
2016-11-26 20:16:05 +0000

    Popup and background fading

--- theme/js/script.js
+++ theme/js/script.js
@@ -1,11 +1,82 @@
-'use strict';
+"use strict";
+
+var Popup = new React.createClass({
+
+	propTypes: {
+		onUserInput: React.PropTypes.func
+	},
+
+	getInitialState: function getInitialState() {
+		return {
+			classes: 'md-modal md-effect-18 md-show'
+		};
+	},
+
+	componentDidMount: function componentDidMount() {
+
+		setTimeout(function () {
+			var elem = document.getElementsByTagName("html")[0];
+			elem.classList.add("md-perspective");
+		}, 25);
+	},
+
+	handleClick: function handleClick() {
+
+		ReactDOM.unmountComponentAtNode(document.getElementById('popup'));
+
+		var elem = document.getElementsByTagName("html")[0];
+		elem.classList.remove("md-perspective");
+
+		if (typeof this.props.onUserInput === 'function') {
+			this.props.onUserInput();
+		}
+	},
+
+	render: function render() {
+		return React.createElement(
+			"div",
+			{ className: this.state.classes, id: "modal-18" },
+			React.createElement(
+				"div",
+				{ className: "md-content" },
+				React.createElement(
+					"h3",
+					null,
+					"Modal Dialog - ",
+					this.props.id
+				),
+				React.createElement(
+					"div",
+					null,
+					React.createElement(
+						"p",
+						null,
+						"This is a modal window. You can do the following things with it:"
+					),
+					React.createElement(
+						"p",
+						null,
+						"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+					),
+					React.createElement(
+						"button",
+						{ className: "md-close", onClick: this.handleClick },
+						"Close"
+					)
+				)
+			)
+		);
+	}
+
+});
 
 var Website = new React.createClass({
 
 	getInitialState: function getInitialState() {
 		return {
 			loaded: false,
-			sites: 'Loading'
+			sites: 'Loading',
+			portfolioClasses: 'portfolio'
 		};
 	},
 
@@ -31,28 +102,58 @@ var Website = new React.createClass({
 		}
 	},
 
+	handleClick: function handleClick(e) {
+
+		e.preventDefault();
+
+		var id = e.target.getAttribute('data-id');
+
+		this.setState({ portfolioClasses: 'portfolio selected' });
+
+		// $(this).addClass('highlight');
+
+		// if ( $('.portfolio').hasClass('selected') ) {
+		// 		// 	$('.portfolio').removeClass('selected');
+		// 		// 	$('.item').removeClass('highlight');
+		// 		// } else {
+		// 			$('.portfolio').addClass('selected');
+		// 			$(this).addClass('highlight');
+		// 		// }
+
+
+		ReactDOM.render(React.createElement(Popup, { id: id, onUserInput: this.popupHasClosed }), document.getElementById('popup'));
+
+		return false;
+	},
+
+	popupHasClosed: function popupHasClosed() {
+		this.setState({ portfolioClasses: 'portfolio' });
+	},
+
 	render: function render() {
 
 		if (this.state.loaded) {
 
-			var siteList = this.state.sites.map(function (site) {
+			var clickHandler = this.handleClick;
+
+			var siteList = this.state.sites.map(function (site, index) {
 
 				var classList = "item " + site.classes;
 				var image = "url('" + site.image + "')";
 
 				return React.createElement(
-					'a',
-					{ target: '_blank', href: site.url, className: classList, style: { backgroundImage: image } },
+					"a",
+					{ onClick: clickHandler, target: "_blank", href: site.url, className: classList, style: { backgroundImage: image }, "data-id": index },
 					React.createElement(
-						'div',
-						{ className: 'inner' },
+						"div",
+						{ className: "inner" },
 						React.createElement(
-							'h3',
+							"h3",
 							null,
 							site.title
 						),
 						React.createElement(
-							'h4',
+							"h4",
 							null,
 							site.tagline
 						)
@@ -61,16 +162,16 @@ var Website = new React.createClass({
 			});
 
 			return React.createElement(
-				'div',
-				{ className: 'portfolio' },
+				"div",
+				{ className: this.state.portfolioClasses },
 				siteList
 			);
 		} else {
 
 			return React.createElement(
-				'h1',
+				"h1",
 				null,
-				'Loading....'
+				"Loading...."
 			);
 		}
 	}
@@ -78,40 +179,3 @@ var Website = new React.createClass({
 });
 
 ReactDOM.render(React.createElement(Website, null), document.getElementById('portfolio'));
-
-// $( document ).ready(function() {
-
-// 	$('.portfolio').packery({
-// 		 percentPosition: true
-// 	});
-
-// 	$(document).on( 'click' , '.item' , function(e) {
-
-// 		e.preventDefault();
-
-// 		// if ( $('.portfolio').hasClass('selected') ) {
-// 		// 	$('.portfolio').removeClass('selected');
-// 		// 	$('.item').removeClass('highlight');
-// 		// } else {
-// 			$('.portfolio').addClass('selected');
-// 			$(this).addClass('highlight');
-// 		// }
-
-// 		$('#modal-18').addClass('md-show');
-
-// 		setTimeout( function() {
-// 			$('html').addClass( 'md-perspective' );
-// 		}, 25 );
-
-// 	} );
-
-// 	$(document).on( 'click' , '.md-modal .md-close' , function() {
-// 		$('.md-modal').removeClass( 'md-show' );
-// 		$('html').removeClass( 'md-perspective' );	
-
-// 		$('.portfolio').removeClass('selected');
-// 		$('.item').removeClass('highlight');
-// 	});
-
-
-// });
--- theme/js/src/script.jsx
+++ theme/js/src/script.jsx
@@ -1,9 +1,71 @@
+var Popup = new React.createClass({
+
+	propTypes: {
+		onUserInput: React.PropTypes.func
+	},
+
+	getInitialState: function() {
+		return {
+            classes: 'md-modal md-effect-18 md-show'
+        };
+	},
+
+	componentDidMount: function () {
+
+		setTimeout( function() {
+			var elem = document.getElementsByTagName("html")[0];
+			elem.classList.add("md-perspective");
+		}, 25 );
+		 		
+	},
+
+	handleClick: function () {
+		
+		ReactDOM.unmountComponentAtNode(document.getElementById('popup'));
+		
+		var elem = document.getElementsByTagName("html")[0];
+		elem.classList.remove("md-perspective");
+
+		if (typeof this.props.onUserInput === 'function') {
+			this.props.onUserInput();
+		}
+
+	},
+
+	render: function () {
+		return (
+			<div className={this.state.classes} id="modal-18">
+    			<div className="md-content">
+    				<h3>Modal Dialog - {this.props.id}</h3>
+    				<div>
+    					<p>This is a modal window. You can do the following things with it:</p>
+    					<p>
+    						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+    						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+    						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+    						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+    						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+    						proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    					</p>
+    					<button className="md-close" onClick={this.handleClick}>Close</button>
+    				</div>
+    			</div>
+    		</div>
+		);
+	}
+
+});
+
+
+
+
 var Website = new React.createClass( {
 
 	getInitialState: function () {
 		return {
 			loaded: false,
-			sites: 'Loading'    
+			sites: 'Loading',
+			portfolioClasses: 'portfolio'    
 		};
 	},
 
@@ -28,17 +90,53 @@ var Website = new React.createClass( {
 
   	},
 
+  	handleClick: function ( e ) {
+
+  		e.preventDefault();
+
+  		var id = e.target.getAttribute('data-id');
+
+  		this.setState( { portfolioClasses: 'portfolio selected' } );
+
+
+  		// $(this).addClass('highlight');
+
+// if ( $('.portfolio').hasClass('selected') ) {
+// 		// 	$('.portfolio').removeClass('selected');
+// 		// 	$('.item').removeClass('highlight');
+// 		// } else {
+// 			$('.portfolio').addClass('selected');
+// 			$(this).addClass('highlight');
+// 		// }
+
+
+
+  		ReactDOM.render(
+			<Popup id={id} onUserInput={this.popupHasClosed} />,
+			document.getElementById('popup')
+		);
+
+  		return false;
+
+  	},
+
+  	popupHasClosed: function () {
+  		this.setState( { portfolioClasses: 'portfolio' } );
+  	},
+
 	render: function () {
 
 		if ( this.state.loaded ) {
 
-			var siteList = this.state.sites.map(function(site) {
+			var clickHandler = this.handleClick;
+
+			var siteList = this.state.sites.map(function(site,index) {
 
 				var classList = "item " + site.classes;
 				var image = "url('" + site.image + "')"
 
 				return (
-					<a target="_blank" href={ site.url } className={classList} style={{backgroundImage: image}}>
+					<a onClick={clickHandler} target="_blank" href={ site.url } className={classList} style={{backgroundImage: image}} data-id={ index }>
 		    			<div className="inner">
 		    				<h3>{ site.title }</h3>
 		    				<h4>{ site.tagline }</h4>
@@ -47,7 +145,7 @@ var Website = new React.createClass( {
 				);
 			});
 
-			return <div className="portfolio">{ siteList }</div>;
+			return <div className={this.state.portfolioClasses}>{ siteList }</div>;
 
 		} else {
 
@@ -64,43 +162,3 @@ ReactDOM.render(
 	<Website />,
 	document.getElementById('portfolio')
 );
-
-
-// $( document ).ready(function() {
-	
-// 	$('.portfolio').packery({
-// 		 percentPosition: true
-// 	});
-
-// 	$(document).on( 'click' , '.item' , function(e) {
-
-// 		e.preventDefault();
-
-// 		// if ( $('.portfolio').hasClass('selected') ) {
-// 		// 	$('.portfolio').removeClass('selected');
-// 		// 	$('.item').removeClass('highlight');
-// 		// } else {
-// 			$('.portfolio').addClass('selected');
-// 			$(this).addClass('highlight');
-// 		// }
-
-// 		$('#modal-18').addClass('md-show');
-
-// 		setTimeout( function() {
-// 			$('html').addClass( 'md-perspective' );
-// 		}, 25 );
-
-// 	} );
-
-// 	$(document).on( 'click' , '.md-modal .md-close' , function() {
-// 		$('.md-modal').removeClass( 'md-show' );
-// 		$('html').removeClass( 'md-perspective' );	
-
-// 		$('.portfolio').removeClass('selected');
-// 		$('.item').removeClass('highlight');
-// 	});
-
-	
-// });
-
-
